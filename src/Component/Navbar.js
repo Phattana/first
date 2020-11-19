d8088a7fb6a63fc58a101cc784da600acf898985
Phattana <phattana.w@almond.group>
2020-11-19 16:40:19 +0700

    update navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,94 +1,20 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import { ReactComponent as Logo } from "../Picture/dw_panel.svg";
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-import { createMuiTheme } from '@material-ui/core/styles';
-import ListItemText from "@material-ui/core/ListItemText";
-// import InboxIcon from '@material-ui/icons/MoveToInbox';
-// import MailIcon from '@material-ui/icons/Mail';
 
-const drawerWidth = 274;
-
-const theme = createMuiTheme({
-    typography: {
-      // In Chinese and Japanese the characters are usually larger,
-      // so a smaller fontsize may be appropriate.
-      fontSize: 120,
-    },
-  });
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    marginLeft: "61"
-  },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    color: "white",
-    background:
-      "linear-gradient(to bottom,#00005b,#01015d,#070566,#100b75,#1d148a,#2e20a5,#3c2abb)",
-    width: drawerWidth
-  },
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3)
-  },
-  
-}));
 
 function PermanentDrawerLeft() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-        anchor="left"
-      >
-        <Logo height="88" width="89" />
-
-        <List>
-          {[
-            "หน้าแรก",
-            "สร้างแบบสอบถาม",
-            "ข้อมูลกลุ่มตัวอย่าง",
-            "การจัดการระบบ",
-            "ช่วยเหลือ"
-          ].map((text, index) => (
-            <ListItem button key={text}>
-              {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
-              <ListItemText primary={text}   />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-      </main>
+    <div>
+      <div>
+        Hello
+      </div>
+      <div>
+        It
+      </div>
+      <div>
+        ME
+      </div>
     </div>
+
   );
 }
 
